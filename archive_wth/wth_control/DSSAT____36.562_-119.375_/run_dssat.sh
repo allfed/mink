c6bcf368137ca72d2ee7544025cee9c9656e8a30
Morgan Rivers <danielmorganrivers@gmail.com>
2022-10-09 14:46:02 +0200

    removed stupid folder

--- archive_wth/wth_control/DSSAT____36.562_-119.375_/run_dssat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./dscsm_compiled_fast.exe n deleteme.v45
-
-# (DMR) this copies the dssat out to a sub-folder so we can look at the individual 
-# cells being run. Can be deleted.
-new_name_postfix=`head RRRR.WTH -n 2 | tail -n 1 | head -c 25 | tail -c 19|tr ' ' '_'`
-cp -r . ../../DSSAT_"$new_name_postfix"
